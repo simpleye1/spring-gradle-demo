2371cc0d912bc26cda04bf6751d525425166c9b3
simpleye1 <simpleye11@qq.com>
2022-02-13 23:34:11 +0800

    gradle test

--- src/main/java/com/thoughtworks/capacity/gtb/blank/CarController.java
+++ src/main/java/com/thoughtworks/capacity/gtb/blank/CarController.java
@@ -1,5 +1,9 @@
 package com.thoughtworks.capacity.gtb.blank;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CarController {
 
   private final CarService carService;
@@ -7,4 +11,10 @@ public class CarController {
   public CarController(CarService carService) {
     this.carService = carService;
   }
+
+  @GetMapping("/")
+  public String HelloWorld() {
+    return "hello world";
+  }
+
 }
---
+++ src/test/java/com/thoughtworks/capacity/gtb/blank/CarControllerTest.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.capacity.gtb.blank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarControllerTest {
+    @Test
+    void justASimpleTest() {
+        assertThat(1).isEqualTo(1);
+    }
+
+}
