15e28beb6eb33d140e55e59de54724b61625ec34
simpleye1 <simpleye11@qq.com>
2022-03-06 13:35:18 +0800

    hello

--- src/main/java/com/thoughtworks/capacity/gtb/blank/BlankApplication.java
+++ src/main/java/com/thoughtworks/capacity/gtb/blank/BlankApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlankApplication {
+
   public static void main(String[] args) {
     SpringApplication.run(BlankApplication.class, args);
   }
